475632c18757bfef5abd1f5f0b37b23f1162f5b5
Maxim Samsonov <m.samsonov@computer.org>
2025-06-22 10:44:25 +0300

    wip: fix coverage reports

--- tests/Logibooks.Core.Tests/Data/AppDbContextTests.cs
+++ tests/Logibooks.Core.Tests/Data/AppDbContextTests.cs
@@ -1,3 +1,28 @@
+// Copyright (C) 2025 Maxim [maxirmx] Samsonov (www.sw.consulting)
+// All rights reserved.
+// This file is a part of Logibooks Core applcation
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS
+// BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
 using NUnit.Framework;
 using Microsoft.EntityFrameworkCore;
 using Logibooks.Core.Data;
@@ -18,20 +43,20 @@ public class AppDbContextTests
     public void CheckSameUser_ReturnsTrue_WhenIdsMatch()
     {
         using var ctx = CreateContext();
-        Assert.True(ctx.CheckSameUser(1, 1));
+        Assert.That(ctx.CheckSameUser(1, 1), Is.True);
     }
 
     [Test]
     public void CheckSameUser_ReturnsFalse_WhenIdsDiffer()
     {
         using var ctx = CreateContext();
-        Assert.False(ctx.CheckSameUser(1, 2));
+        Assert.That(ctx.CheckSameUser(1, 2), Is.False);
     }
 
     [Test]
     public void CheckSameUser_ReturnsFalse_WhenCuidZero()
     {
         using var ctx = CreateContext();
-        Assert.False(ctx.CheckSameUser(1, 0));
+        Assert.That(ctx.CheckSameUser(1, 0), Is.False);
     }
 }
