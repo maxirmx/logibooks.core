be7b7505b69bd785af9da47a97652c038fe2fd3d
Maxim Samsonov <maxirmx@sw.consulting>
2025-07-01 21:28:51 +0300

    Update RegisterViewItem.cs

--- Logibooks.Core/RestModels/RegisterViewItem.cs
+++ Logibooks.Core/RestModels/RegisterViewItem.cs
@@ -6,7 +6,6 @@ public class RegisterViewItem
     public int Id { get; set; }
     public string FileName { get; set; } = string.Empty;
     public DateTime Date { get; set; }
-    public List<Order> Orders { get; set; } = [];
     public int OrdersTotal { get; set; }
     public Dictionary<int, int> OrdersByStatus { get; set; } = new();
 }
