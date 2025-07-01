f80dd4159f5d3f13632dafd0ca0de5a5670fe2bb
Maxim Samsonov <maxirmx@sw.consulting>
2025-07-01 21:33:49 +0300

    Add order counts to register view (#26)

---
+++ Logibooks.Core/RestModels/RegisterViewItem.cs
@@ -0,0 +1,11 @@
+using Logibooks.Core.Models;
+namespace Logibooks.Core.RestModels;
+
+public class RegisterViewItem
+{
+    public int Id { get; set; }
+    public string FileName { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+    public int OrdersTotal { get; set; }
+    public Dictionary<int, int> OrdersByStatus { get; set; } = new();
+}
