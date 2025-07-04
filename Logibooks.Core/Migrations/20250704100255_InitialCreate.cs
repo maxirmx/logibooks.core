0022ca56241a97fa3c1638dbf9be3a2e1b710538
Maxim Samsonov <m.samsonov@computer.org>
2025-07-04 20:58:13 +0300

    wip: reset migration history

---
+++ Logibooks.Core/Migrations/20250704100255_InitialCreate.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Logibooks.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "registers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    filename = table.Column<string>(type: "text", nullable: false),
+                    dtime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_registers", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "roles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    title = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_roles", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "statuses",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    title = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_statuses", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    first_name = table.Column<string>(type: "text", nullable: false),
+                    last_name = table.Column<string>(type: "text", nullable: false),
+                    patronymic = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    password = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "orders",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    register_id = table.Column<int>(type: "integer", nullable: false),
+                    status_id = table.Column<int>(type: "integer", nullable: false),
+                    row_number = table.Column<int>(type: "integer", nullable: false),
+                    order_number = table.Column<string>(type: "text", nullable: true),
+                    invoice_date = table.Column<string>(type: "text", nullable: true),
+                    sticker = table.Column<string>(type: "text", nullable: true),
+                    shk = table.Column<string>(type: "text", nullable: true),
+                    sticker_code = table.Column<string>(type: "text", nullable: true),
+                    ext_id = table.Column<string>(type: "text", nullable: true),
+                    tn_ved = table.Column<string>(type: "text", nullable: true),
+                    site_article = table.Column<string>(type: "text", nullable: true),
+                    heel_height = table.Column<string>(type: "text", nullable: true),
+                    size = table.Column<string>(type: "text", nullable: true),
+                    product_name = table.Column<string>(type: "text", nullable: true),
+                    description = table.Column<string>(type: "text", nullable: true),
+                    gender = table.Column<string>(type: "text", nullable: true),
+                    brand = table.Column<string>(type: "text", nullable: true),
+                    fabric_type = table.Column<string>(type: "text", nullable: true),
+                    composition = table.Column<string>(type: "text", nullable: true),
+                    lining = table.Column<string>(type: "text", nullable: true),
+                    insole = table.Column<string>(type: "text", nullable: true),
+                    sole = table.Column<string>(type: "text", nullable: true),
+                    country = table.Column<string>(type: "text", nullable: true),
+                    factory_address = table.Column<string>(type: "text", nullable: true),
+                    unit = table.Column<string>(type: "text", nullable: true),
+                    weight_kg = table.Column<string>(type: "text", nullable: true),
+                    quantity = table.Column<string>(type: "text", nullable: true),
+                    unit_price = table.Column<string>(type: "text", nullable: true),
+                    currency = table.Column<string>(type: "text", nullable: true),
+                    barcode = table.Column<string>(type: "text", nullable: true),
+                    declaration = table.Column<string>(type: "text", nullable: true),
+                    product_link = table.Column<string>(type: "text", nullable: true),
+                    recipient_name = table.Column<string>(type: "text", nullable: true),
+                    recipient_inn = table.Column<string>(type: "text", nullable: true),
+                    passport_number = table.Column<string>(type: "text", nullable: true),
+                    pinfl = table.Column<string>(type: "text", nullable: true),
+                    recipient_address = table.Column<string>(type: "text", nullable: true),
+                    contact_phone = table.Column<string>(type: "text", nullable: true),
+                    box_number = table.Column<string>(type: "text", nullable: true),
+                    supplier = table.Column<string>(type: "text", nullable: true),
+                    supplier_inn = table.Column<string>(type: "text", nullable: true),
+                    category = table.Column<string>(type: "text", nullable: true),
+                    subcategory = table.Column<string>(type: "text", nullable: true),
+                    personal_data = table.Column<string>(type: "text", nullable: true),
+                    customs_clearance = table.Column<string>(type: "text", nullable: true),
+                    duty_payment = table.Column<string>(type: "text", nullable: true),
+                    other_reason = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_orders", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_orders_registers_register_id",
+                        column: x => x.register_id,
+                        principalTable: "registers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_orders_statuses_status_id",
+                        column: x => x.status_id,
+                        principalTable: "statuses",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_roles",
+                columns: table => new
+                {
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    role_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_roles", x => new { x.user_id, x.role_id });
+                    table.ForeignKey(
+                        name: "FK_user_roles_roles_role_id",
+                        column: x => x.role_id,
+                        principalTable: "roles",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_user_roles_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "roles",
+                columns: new[] { "id", "name", "title" },
+                values: new object[,]
+                {
+                    { 1, "logist", "Логист" },
+                    { 2, "administrator", "Администратор" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "statuses",
+                columns: new[] { "id", "name", "title" },
+                values: new object[] { 1, "loaded", "Загружен" });
+
+            migrationBuilder.InsertData(
+                table: "users",
+                columns: new[] { "id", "email", "first_name", "last_name", "password", "patronymic" },
+                values: new object[] { 1, "maxirmx@sw.consulting", "Maxim", "Samsonov", "$2b$12$eOXzlwFzyGVERe0sNwFeJO5XnvwsjloUpL4o2AIQ8254RT88MnsDi", "" });
+
+            migrationBuilder.InsertData(
+                table: "user_roles",
+                columns: new[] { "role_id", "user_id" },
+                values: new object[] { 2, 1 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orders_register_id",
+                table: "orders",
+                column: "register_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orders_status_id",
+                table: "orders",
+                column: "status_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_roles_role_id",
+                table: "user_roles",
+                column: "role_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "orders");
+
+            migrationBuilder.DropTable(
+                name: "user_roles");
+
+            migrationBuilder.DropTable(
+                name: "registers");
+
+            migrationBuilder.DropTable(
+                name: "statuses");
+
+            migrationBuilder.DropTable(
+                name: "roles");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
