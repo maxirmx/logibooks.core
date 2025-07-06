0280f215b6752cde91933b509fe0ab1fe78fa173
Maxim Samsonov <m.samsonov@computer.org>
2025-07-06 17:14:00 +0300

    wip: use tamdoc

--- Logibooks.Core/Controllers/AltaController.cs
+++ Logibooks.Core/Controllers/AltaController.cs
@@ -1,7 +1,32 @@
+// Copyright (C) 2025 Maxim [maxirmx] Samsonov (www.sw.consulting)
+// All rights reserved.
+// This file is a part of Logibooks Core application
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS
+// BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+
 using Logibooks.Core.Data;
-using Logibooks.Core.Models;
 using Logibooks.Core.Services;
 using Logibooks.Core.Authorization;
 using Logibooks.Core.RestModels;
@@ -29,8 +54,11 @@ public class AltaController(
 
         var urls = new List<string>
         {
-            "https://example.com/alta1",
-            "https://example.com/alta2"
+            "https://www.alta.ru/tamdoc/11pr0971/",
+            "https://www.alta.ru/tamdoc/22ps0311/",
+            "https://www.alta.ru/tamdoc/10sr0317/",
+            "https://www.alta.ru/tamdoc/22ps0312/",
+            "https://www.alta.ru/tamdoc/10sr0318/"
         };
 
         var (items, exceptions) = await AltaParser.ParseAsync(urls, _httpClient);
