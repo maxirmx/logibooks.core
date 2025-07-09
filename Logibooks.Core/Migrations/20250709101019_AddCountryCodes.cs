34a9e3ae5f4ca185d6bb9a5e6cc77540de75ac14
Maxim Samsonov <maxirmx@sw.consulting>
2025-07-09 13:24:55 +0300

    Add CountryCode model and migration

---
+++ Logibooks.Core/Migrations/20250709101019_AddCountryCodes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Logibooks.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCountryCodes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "country_codes",
+                columns: table => new
+                {
+                    iso_numeric = table.Column<short>(type: "smallint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    iso_alpha2 = table.Column<string>(type: "text", nullable: false),
+                    name_en_short = table.Column<string>(type: "text", nullable: true),
+                    name_en_formal = table.Column<string>(type: "text", nullable: true),
+                    name_en_official = table.Column<string>(type: "text", nullable: true),
+                    name_en_cldr = table.Column<string>(type: "text", nullable: true),
+                    name_ru_short = table.Column<string>(type: "text", nullable: true),
+                    name_ru_formal = table.Column<string>(type: "text", nullable: true),
+                    name_ru_official = table.Column<string>(type: "text", nullable: true),
+                    loaded_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_country_codes", x => x.iso_numeric);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "country_codes");
+        }
+    }
+}
---
+++ Logibooks.Core/Models/CountryCode.cs
@@ -0,0 +1,63 @@
+// Copyright (C) 2025 Maxim [maxirmx] Samsonov (www.sw.consulting)
+// All rights reserved.
+// This file is a part of Logibooks Core application
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS
+// BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Logibooks.Core.Models;
+
+[Table("country_codes")]
+public class CountryCode
+{
+    [Column("iso_numeric")]
+    public short IsoNumeric { get; set; }
+
+    [Column("iso_alpha2")]
+    public required string IsoAlpha2 { get; set; }
+
+    [Column("name_en_short")]
+    public string? NameEnShort { get; set; }
+
+    [Column("name_en_formal")]
+    public string? NameEnFormal { get; set; }
+
+    [Column("name_en_official")]
+    public string? NameEnOfficial { get; set; }
+
+    [Column("name_en_cldr")]
+    public string? NameEnCldr { get; set; }
+
+    [Column("name_ru_short")]
+    public string? NameRuShort { get; set; }
+
+    [Column("name_ru_formal")]
+    public string? NameRuFormal { get; set; }
+
+    [Column("name_ru_official")]
+    public string? NameRuOfficial { get; set; }
+
+    [Column("loaded_at")]
+    public DateTime LoadedAt { get; set; } = DateTime.UtcNow;
+}
