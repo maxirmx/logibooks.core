7b5251e0cab390f3a722b7f2e34fd6cde7d46e00
Maxim Samsonov <m.samsonov@computer.org>
2025-07-14 02:28:03 +0300

    snapshot

--- Logibooks.Core/RestModels/OrderStatusDto.cs
+++ Logibooks.Core/RestModels/OrderStatusDto.cs
@@ -5,7 +5,6 @@ using Logibooks.Core.Models;
 public class OrderStatusDto
 {
     public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
     public string Title { get; set; } = string.Empty;
 
     public OrderStatusDto() {}
--- Logibooks.Core/RestModels/RegisterViewItem.cs
+++ Logibooks.Core/RestModels/RegisterViewItem.cs
@@ -29,7 +29,6 @@ public class RegisterViewItem
 {
     public int Id { get; set; }
     public int CompanyId { get; set; }
-    public string CompanyName { get; set; } = string.Empty;
     public string FileName { get; set; } = string.Empty;
     public DateTime Date { get; set; }
     public int OrdersTotal { get; set; }
